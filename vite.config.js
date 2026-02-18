3a4efcae0e434e0e8cf9d00fad904e790d4cbdde
Harel Panker <harelpanker@gmail.com>
2026-02-18 12:46:49 +0100

    Fix base path to match repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/remepy-vite/",
+  base: "/remepy_static_content/",
   build: {
     rollupOptions: {
       input: {
