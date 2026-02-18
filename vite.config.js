7957e6f77b05abbb074f496fe954c043ca0a21b0
Harel Panker <harelpanker@gmail.com>
2026-02-18 12:43:15 +0100

    fix-for-the-build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/remepy-vite/',
-})
+  base: "/remepy-vite/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pacing: resolve(__dirname, "articles/pacing.html"),
+        mindfulness: resolve(__dirname, "articles/mindfulness.html"),
+        relaxation: resolve(__dirname, "articles/relaxation.html"),
+        "healthy-eating": resolve(__dirname, "articles/healthy-eating.html"),
+        starter: resolve(__dirname, "articles/starter.html"),
+      },
+    },
+  },
+});
